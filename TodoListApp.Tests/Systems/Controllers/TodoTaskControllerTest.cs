33101e686b25e033ca2aaf2bb0592e181025f4b9
thatogit <36532635+thatogit@users.noreply.github.com>
2024-04-16 07:48:32 +0200

    test and web api

---
+++ TodoListApp.Tests/Systems/Controllers/TodoTaskControllerTest.cs
@@ -0,0 +1,70 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TodoListApp.Api.Controllers;
+using TodoListApp.Api.Services.Interface;
+
+namespace TodoListApp.Tests.Systems.Controllers;
+
+public class TodoTaskControllerTest
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnStatusCode200()
+    {
+        //Arrange
+        var mockTaskService = new Mock<ITaskService>();
+        mockTaskService.Setup(service => service.GetAllTasks()).ReturnsAsync(new List<Task>());
+        var todoController = new TodoTaskController(mockTaskService.Object);
+        //Act
+        var result = (OkObjectResult)await todoController.Get();
+
+        //Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_InvokeService()
+    {
+        //Arrange
+        var mockTaskService = new Mock<ITaskService>();
+        mockTaskService.Setup(service => service.GetAllTasks()).ReturnsAsync(new List<Task>());
+        var todoController = new TodoTaskController(mockTaskService.Object);
+        //Act
+        var result = (OkObjectResult)await todoController.Get();
+
+        //Assert
+        mockTaskService.Verify(service=>service.GetAllTasks(),Times.Once);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_ReturnListOfTask()
+    {
+        //Arrange
+        var mockTaskService = new Mock<ITaskService>();
+        mockTaskService.Setup(service => service.GetAllTasks()).ReturnsAsync(new List<Task>());
+        
+        var todoController = new TodoTaskController(mockTaskService.Object);
+        //Act
+        var result = (OkObjectResult) await todoController.Get();
+        
+        //Assert
+        result.Should().BeOfType<OkObjectResult>();
+
+        result.Value.Should().BeOfType<List<Task>>();
+    }
+    
+    [Fact]
+    public async Task Get_OnNoTask_ReturnNoFound()
+    {
+        //Arrange
+        var mockTaskService = new Mock<ITaskService>();
+        mockTaskService.Setup(service => service.GetAllTasks()).ReturnsAsync(new List<Task>());
+        
+        var todoController = new TodoTaskController(mockTaskService.Object);
+        //Act
+        var result = (NotFoundResult) await todoController.Get();
+        
+        //Assert
+        result.Should().BeOfType<NotFoundResult>();
+    }
+}
---
+++ TodoListApp.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TodoListApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
